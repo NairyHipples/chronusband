6d19aef21c31b9d7268aecc27a4b6482b15d066e
Jonatan Östling <jonatan.ostling@gmail.com>
2016-12-09 10:32:13 +0100

    Trying to fix buggy URL bar jumpiness on phone

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,10 @@ $(document).ready(function() {
   setTimeout(function(){
     $('body').addClass('loaded');
   }, 2000);
+  var bg = jQuery("#home, #band, #contact");
+  jQuery(window).resize("resizeBackground");
+  function resizeBackground() {
+    bg.height(jQuery(window).height() + 60);
+  }
+  resizeBackground();
 }); // end DOC ready
