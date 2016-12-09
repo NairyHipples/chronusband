fe74fe75128404291d66eb3f8173f5677df2b005
Jonatan Östling <jonatan.ostling@gmail.com>
2016-12-09 10:47:47 +0100

    Trying to fix the problem

--- scripts/main.js
+++ scripts/main.js
@@ -8,10 +8,4 @@ $(document).ready(function() {
   setTimeout(function(){
     $('body').addClass('loaded');
   }, 2000);
-  var bg = jQuery("#home, #band, #contact");
-  jQuery(window).resize("resizeBackground");
-  function resizeBackground() {
-    bg.height(jQuery(window).height() - 60);
-  }
-  resizeBackground();
 }); // end DOC ready
